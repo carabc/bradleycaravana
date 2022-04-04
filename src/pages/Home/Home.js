afde8302935c5cf4906d7122479a669325d1b08a
bcaravana <bcaravana@maximgrp.com>
2022-04-04 08:23:37 -0400

    Added breakpoints and styles for smaller screens on navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,19 @@
-import React from "react";
+/** @jsx jsx */
+// React and basic dependencies
+import React, { useState, useEffect } from "react";
+
+// React router dom
+import { Link } from "react-router-dom";
+
+// Material UI Styles
+import { jsx, css } from "@emotion/react";
+import { styled } from "@mui/system";
+
+// Material UI Theme
+import { useTheme } from "@mui/styles";
 
 const Home = () => {
+  const theme = useTheme();
   return <div>Home</div>;
 };
 
